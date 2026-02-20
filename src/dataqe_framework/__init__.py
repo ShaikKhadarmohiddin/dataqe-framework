01198d3ad49ea3f0b6c3bbd53bc75dee91afdf1b
khadarmohiddin.shai <khadarmohiddin.shaik@apree.health>
2026-02-20 16:53:35 +0530

    Mark as Production/Stable and bump to 0.2.1

--- src/dataqe_framework/__init__.py
+++ src/dataqe_framework/__init__.py
@@ -1,7 +1,7 @@
 from .validator import DataValidator
 from .credentials_extractor import CredentialsExtractor
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = ["DataValidator", "CredentialsExtractor"]
 
